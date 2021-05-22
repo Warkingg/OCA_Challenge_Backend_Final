8da8c7e4770d42251bfdabb084a26a797262f130
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-22 20:15:12 +0700

    add LevelController

--- src/main/java/com/example/quizappdrycode/controller/LevelController.java
+++ src/main/java/com/example/quizappdrycode/controller/LevelController.java
@@ -1,4 +1,29 @@
 package com.example.quizappdrycode.controller;
 
+import com.example.quizappdrycode.model.Level;
+import com.example.quizappdrycode.service.ILevelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/levels")
 public class LevelController {
+    @Autowired
+    private ILevelService levelService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Level>> getLevels() {
+        return new ResponseEntity<>(levelService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Level> getLevel(@PathVariable ("id") Long id) {
+        Optional<Level> levelOptional = levelService.findById(id);
+        return new ResponseEntity<>(levelOptional.get(), HttpStatus.OK);
+    }
 }
