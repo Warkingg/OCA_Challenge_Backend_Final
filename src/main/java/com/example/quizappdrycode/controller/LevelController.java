31e6f33cba501ac9f813c921bbee9f6a516b86ca
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-22 18:07:32 +0700

    update LevelController

--- src/main/java/com/example/quizappdrycode/controller/LevelController.java
+++ src/main/java/com/example/quizappdrycode/controller/LevelController.java
@@ -1,4 +1,38 @@
 package com.example.quizappdrycode.controller;
 
+import com.example.quizappdrycode.model.Level;
+import com.example.quizappdrycode.model.Topic;
+import com.example.quizappdrycode.service.ILevelService;
+import com.example.quizappdrycode.service.ITopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/levels")
 public class LevelController {
+    @Autowired
+    private ILevelService levelService;
+
+    @Autowired
+    private ITopicService topicService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Level>> showLevels(){
+        Iterable<Level> levels = levelService.findAll();
+        return new ResponseEntity<>(levels, HttpStatus.OK);
+    }
+
+    @GetMapping("/findLevelByTopic/{id}")
+    public ResponseEntity<Iterable<Level>> findAllByTopic(@PathVariable Long id) {
+        Optional<Topic> topicOptional = topicService.findById(id);
+        if (!topicOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Iterable<Level> levels = levelService.findAllByTopic(topicOptional.get());
+        return new ResponseEntity<>(levels, HttpStatus.OK);
+    }
 }
