b7a47550b650e392027e8465d5693f562e7c8821
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-22 20:44:04 +0700

    lởm

--- src/main/java/com/example/quizappdrycode/controller/LevelController.java
+++ src/main/java/com/example/quizappdrycode/controller/LevelController.java
@@ -1,37 +1,32 @@
 package com.example.quizappdrycode.controller;
 
 import com.example.quizappdrycode.model.Level;
-import com.example.quizappdrycode.model.Topic;
 import com.example.quizappdrycode.service.ILevelService;
-import com.example.quizappdrycode.service.ITopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/levels")
 public class LevelController {
+
     @Autowired
     private ILevelService levelService;
 
-    @Autowired
-    private ITopicService topicService;
-
     @GetMapping
-    public ResponseEntity<Iterable<Level>> showLevels(){
-        Iterable<Level> levels = levelService.findAll();
-        return new ResponseEntity<>(levels, HttpStatus.OK);
+    public ResponseEntity<Iterable<Level>> getLevels() {
+        return new ResponseEntity<>(levelService.findAll(), HttpStatus.OK);
     }
 
-//    @GetMapping("/findLevelByTopic/{id}")
-//    public ResponseEntity<Iterable<Level>> findAllByTopic(@PathVariable("id") Long id) {
-//        Optional<Topic> topicOptional = topicService.findById(id);
-//        if (!topicOptional.isPresent()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        Iterable<Level> levels = levelService.findAllByTopic(topicOptional.get());
-//        return new ResponseEntity<>(levels, HttpStatus.OK);
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Level> getLevel(@PathVariable ("id") Long id) {
+        Optional<Level> levelOptional = levelService.findById(id);
+        return new ResponseEntity<>(levelOptional.get(), HttpStatus.OK);
+    }
+
+//    findAllByTopic lỗi như chó
 }
