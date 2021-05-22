600b70f5e87d6afed5891dc4f25303105302770c
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-05-22 19:14:54 +0700

    Update TypeOfQuestionController.java
    Change name from TypeOfQuestionController to QuestionTypeController

--- src/main/java/com/example/quizappdrycode/controller/TypeOfQuestionController.java
+++ src/main/java/com/example/quizappdrycode/controller/TypeOfQuestionController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
-public class TypeOfQuestionController {
+public class QuestionTypeController {
     @Autowired
     private TypeOfQuestionService typeOfQuestionService;
 
