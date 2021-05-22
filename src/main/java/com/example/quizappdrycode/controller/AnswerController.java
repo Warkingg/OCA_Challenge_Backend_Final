108e86d25745a8eb69430cc5e5d4c0e64bb92df9
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-22 19:24:13 +0700

    add api get answer by question

--- src/main/java/com/example/quizappdrycode/controller/AnswerController.java
+++ src/main/java/com/example/quizappdrycode/controller/AnswerController.java
@@ -1,4 +1,42 @@
 package com.example.quizappdrycode.controller;
 
+import com.example.quizappdrycode.model.Answer;
+import com.example.quizappdrycode.model.Question;
+import com.example.quizappdrycode.service.IAnswerService;
+import com.example.quizappdrycode.service.IQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
 public class AnswerController {
+
+    @Autowired
+    private IAnswerService iAnswerService;
+
+    @Autowired
+    private IQuestionService iQuestionService;
+
+    @PostMapping("/answers/{id}")
+    public ResponseEntity<Answer> createAnswer(@RequestBody Answer answer, @PathVariable Long id) {
+        Optional<Question> questionOptional = iQuestionService.findById(id);
+        if (!questionOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(iAnswerService.save(answer), HttpStatus.OK);
+    }
+
+    @GetMapping("/answers/{id}")
+    public ResponseEntity<Iterable<Answer>> showAllAnswerByQuestion(@PathVariable Long id) {
+        Optional<Question> questionOptional = iQuestionService.findById(id);
+        if (!questionOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Iterable<Answer> answers = iAnswerService.findALlAnswerByQuestion(questionOptional.get());
+        return new ResponseEntity<>(answers, HttpStatus.OK);
+    }
 }
