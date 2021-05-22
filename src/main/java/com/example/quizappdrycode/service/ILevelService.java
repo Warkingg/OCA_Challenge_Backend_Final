eed34236d3fd25435e1564b41c84e8db74c2e41c
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-22 17:11:16 +0700

    add function to service

---
+++ src/main/java/com/example/quizappdrycode/service/ILevelService.java
@@ -0,0 +1,8 @@
+package com.example.quizappdrycode.service;
+
+import com.example.quizappdrycode.model.Level;
+import com.example.quizappdrycode.model.Topic;
+
+public interface ILevelService extends IGeneralService<Level> {
+    Iterable<Level> findAllByTopic(Topic topic);
+}
--- src/main/java/com/example/quizappdrycode/service/IQuestionService.java
+++ src/main/java/com/example/quizappdrycode/service/IQuestionService.java
@@ -1,7 +1,10 @@
 package com.example.quizappdrycode.service;
 
+import com.example.quizappdrycode.model.Level;
 import com.example.quizappdrycode.model.Question;
 
 public interface IQuestionService extends IGeneralService<Question> {
     Iterable<Question> findAll(int page, int size);
+
+    Iterable<Question> findAllByLevel(Level level);
 }
