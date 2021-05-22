dd508e7b7ccd9a7bd28a785cb72a6cc63829a0ce
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-22 19:44:40 +0700

    add QuizController

--- src/main/java/com/example/quizappdrycode/service/Impl/QuizService.java
+++ src/main/java/com/example/quizappdrycode/service/Impl/QuizService.java
@@ -1,4 +1,35 @@
 package com.example.quizappdrycode.service.Impl;
 
-public class QuizService {
+import com.example.quizappdrycode.model.Quiz;
+import com.example.quizappdrycode.repository.IQuizRepository;
+import com.example.quizappdrycode.service.IQuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class QuizService implements IQuizService {
+    @Autowired
+    private IQuizRepository quizRepository;
+
+    @Override
+    public Iterable<Quiz> findAll() {
+        return quizRepository.findAll();
+    }
+
+    @Override
+    public Optional<Quiz> findById(Long id) {
+        return quizRepository.findById(id);
+    }
+
+    @Override
+    public Quiz save(Quiz quiz) {
+        return quizRepository.save(quiz);
+    }
+
+    @Override
+    public void remove(Long id) {
+        quizRepository.deleteById(id);
+    }
 }
