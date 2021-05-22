98385fa127bd08b8b36dde61e846b43d92754616
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-05-22 19:28:42 +0700

    Update TypeOfQuestionService.java
    Change name to QuestionType

--- src/main/java/com/example/quizappdrycode/service/Impl/TypeOfQuestionService.java
+++ src/main/java/com/example/quizappdrycode/service/Impl/TypeOfQuestionService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class TypeOfQuestionService implements ITypeOfQuestionService {
+public class QuestionTypeService implements IQuestionTypeService {
 
     @Autowired
     private IQuestionTypeRepository iQuestionTypeRepository;
