9455b229a98c62a697f857401a4bb4fff2b85462
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-25 10:19:03 +0700

    add verification token service

--- src/main/java/com/example/quizappdrycode/service/IVerificationTokenService.java
+++ src/main/java/com/example/quizappdrycode/service/IVerificationTokenService.java
@@ -1,4 +1,9 @@
 package com.example.quizappdrycode.service;
 
+import com.example.quizappdrycode.model.VerificationToken;
+
 public interface IVerificationTokenService {
+    VerificationToken findByToken(String token);
+
+    void save(VerificationToken token);
 }
