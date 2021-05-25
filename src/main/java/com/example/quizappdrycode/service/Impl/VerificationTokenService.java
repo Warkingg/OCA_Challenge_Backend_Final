4f02e616e003b3ec6c739995f68b5e8b14551b0b
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-25 10:20:26 +0700

    add verification token service

--- src/main/java/com/example/quizappdrycode/service/Impl/VerificationTokenService.java
+++ src/main/java/com/example/quizappdrycode/service/Impl/VerificationTokenService.java
@@ -1,4 +1,23 @@
 package com.example.quizappdrycode.service.Impl;
 
-public class VerificationTokenService {
+import com.example.quizappdrycode.model.VerificationToken;
+import com.example.quizappdrycode.repository.IVerificationTokenRepository;
+import com.example.quizappdrycode.service.IVerificationTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VerificationTokenService implements IVerificationTokenService {
+    @Autowired
+    private IVerificationTokenRepository verificationTokenRepository;
+
+    @Override
+    public VerificationToken findByToken(String token) {
+        return verificationTokenRepository.findByToken(token);
+    }
+
+    @Override
+    public void save(VerificationToken token) {
+        verificationTokenRepository.save(token);
+    }
 }
